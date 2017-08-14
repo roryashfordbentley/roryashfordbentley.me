4692dbdc8d7b7fef0ea8e5285cf040164ee437d1
roikles <roikles@roikless-iMac.local>
2017-08-14 15:56:13 +0100

    split Fetch calls into seperate components

--- app/components/featured-project/index.js
+++ app/components/featured-project/index.js
@@ -5,39 +5,62 @@ import ReactDOM from 'react-dom';
 
 class FeaturedProject extends React.Component {
 
-    componentDidMount() {
-        //console.log(this.props);
+    constructor(props) {
+        super(props);
+        this.projectsUrl = 'http://localhost/portfolio/wordpress/wp-json/wp/v2/projects'
+        this.state = {
+            title: '',
+            taxonomy: '',
+            url: '',
+            img: ''
+        }
     }
 
-    aspectRatioImg(){
-        let img = '';
+    getProject(projectId) {
+        let url = this.projectsUrl + '/' + projectId + '/?_embed';
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            //console.log(data);
+
+            this.setState({ 
+                title: data.title.rendered,
+                taxonomy: data._embedded['wp:term'][0][0].name,
+                url: 'http://localhost/portfolio/tbc',
+                img: this.aspectRatioImg(data)
+            });
+        });
+    }
+
+    aspectRatioImg(data){
+        let img = data.acf.cover_image_landscape.sizes.landscape_large;
         if(this.props.aspect === 'portrait') {
-            img = this.props.img.sizes.portrait_large;
-        } else {
-            img = this.props.img.sizes.landscape_large;
+            img = data.acf.cover_image_portrait.sizes.portrait_large;
         }
 
         return img;
     }
 
     aspectRatioGrid(){
-        let gridClass = '';
+        let gridClass = 'gi--1-1  gi--2-3--m';
         if(this.props.aspect === 'portrait') {
             gridClass = 'gi--1-1  gi--1-3--m';
-        } else {
-            gridClass = 'gi--1-1  gi--2-3--m';
-        }
+        } 
 
         return gridClass;
+    }
+
+    componentWillMount() {
+        this.getProject(this.props.id);
     }
 
     render() {
         return (
             <div className={this.aspectRatioGrid()}>
                 <div className="featured-project">
-                    <img className="featured-project__img" src={this.aspectRatioImg()} />
-                    <span className="featured-project__cat">{this.props.category}</span>
-                    <h2 className="featured-project__title">{this.props.title}</h2>
+                    <img className="featured-project__img" src={this.state.img} />
+                    <span className="featured-project__cat">{this.state.taxonomy}</span>
+                    <h2 className="featured-project__title">{this.state.title}</h2>
                 </div>
             </div>
         );
--- app/components/featured-projects/index.js
+++ app/components/featured-projects/index.js
@@ -9,54 +9,27 @@ class FeaturedProjects extends React.Component {
     constructor(props) {
         super(props);
         this.postListUrl = 'http://localhost/portfolio/wordpress/wp-json/wp/v2/pages/5', // homepage
-        this.projectsUrl = 'http://localhost/portfolio/wordpress/wp-json/wp/v2/projects' //projects
         this.state = {
             featuredProjects: []
         }
     }
 
-    /**
-     * Get List Of Featured Projects
-     *
-     * The data is pulled from ACF which is set in WP Admin
-     * in the Home page. 
-     */
     getListOfProjects(url) {
         fetch(url)
         .then((response) => response.json())
         .then((data) => {
             let projectsData = data.acf.featured_projects;
-            console.log(projectsData); // order is correct here
-            /**
-             * Fetch the actual posts
-             */
-            this.projects = [];
             
-            for(let i = 0; i < projectsData.length; i++){
-                fetch(this.projectsUrl + '/' + projectsData[i].featured_project + '/?_embed')
-                .then((response) => response.json())
-                .then((data) => {
-                    data.aspect_ratio = projectsData[i].aspect_ratio;
-                    this.projects.push(data);
-
-                    if(i === projectsData.length-1){
-                        this.setState({ 
-                            featuredProjects: this.projects 
-                        });
-                    }
-                });
-            }
+            this.setState({ 
+                featuredProjects: projectsData
+            });
         }).catch(function(err) {
             console.log(err);
         });
     }
-    
+
     componentWillMount() {
         this.getListOfProjects(this.postListUrl);
-    }
-
-    componentDidUpdate() {
-        //console.log(this.state.featuredProjects);
     }
 
     render() {
@@ -66,16 +39,9 @@ class FeaturedProjects extends React.Component {
                     <div className="g">
                         {
                             this.state.featuredProjects.map((project,index) => {
-                                let cat = project._embedded['wp:term'][0][0].name;
-                                let title = project.title.rendered;
-                                let imgAspectRatio = project.aspect_ratio;
-                                let img = project.acf.cover_image_landscape;
-
-                                if(imgAspectRatio === 'portrait'){
-                                    img = project.acf.cover_image_portrait;
-                                }
-                                
-                                return <FeaturedProject key={index} category={cat} title={title} aspect={imgAspectRatio} img={img} />
+                                let id = project.featured_project;
+                                let aspectRatio = project.aspect_ratio;
+                                return <FeaturedProject key={index} id={id} aspect={aspectRatio} />
                             })
                         }
                     </div>
