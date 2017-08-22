122d6a59f15a6c3d26c30ad3fa50e07ae6d4bc88
roikles <roikles@roikless-iMac.local>
2017-08-22 11:11:12 +0100

    refactored hamburger component to remove padding

--- app/components/hamburger/index.js
+++ app/components/hamburger/index.js
@@ -3,18 +3,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Key extends React.Component {
+class Hamburger extends React.Component {
     render() {
         return (
             <button id="hamburger" onClick={this.props.onClick} className={this.props.toggle === "open" ? "hamburger hamburger--open" : "hamburger"}>
                 <svg className="hamburger__svg" viewBox="0 0 100 100">
-                    <line className="hamburger__line  hamburger__line--1" x1="20" y1="50" x2="80" y2="50" />
-                    <line className="hamburger__line  hamburger__line--2" x1="20" y1="50" x2="80" y2="50" />
-                    <line className="hamburger__line  hamburger__line--3" x1="20" y1="50" x2="80" y2="50" /> 
+                    <line className="hamburger__line  hamburger__line--1" x1="0" y1="50" x2="100" y2="50" />
+                    <line className="hamburger__line  hamburger__line--2" x1="0" y1="50" x2="100" y2="50" />
+                    <line className="hamburger__line  hamburger__line--3" x1="0" y1="50" x2="100" y2="50" /> 
                 </svg>
             </button>
         );
     }
 }
 
-export default Key;
+export default Hamburger;
