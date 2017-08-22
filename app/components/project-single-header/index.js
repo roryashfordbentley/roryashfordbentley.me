a979b381e1206f8e6df441c45693415076f006f1
roikles <roikles@roikless-iMac.local>
2017-08-22 12:30:39 +0100

    Added link to project header taxonomy

--- app/components/project-single-header/index.js
+++ app/components/project-single-header/index.js
@@ -4,6 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '../button';
 import RightArrow from '../icons/right-arrow';
+import {
+    BrowserRouter,
+    Route,
+    Link
+} from 'react-router-dom';
 
 class ProjectHeader extends React.Component {
     render() {
@@ -11,7 +16,8 @@ class ProjectHeader extends React.Component {
             <section className="project-header">
                 <div className="project-header__text">
                     <span className="project-header__taxonomy">
-                        Projects/ <span className="project-header__taxonomy--bold"> {this.props.taxonomy}</span>
+                        <Link to="/projects">Projects/ </Link>
+                        <span className="project-header__taxonomy--bold"> {this.props.taxonomy}</span>
                     </span>
                     <h1 className="project-header__title">{this.props.title}</h1>
                 </div>
